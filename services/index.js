0c81a93907ac912ee7724e93ff939a0633c3a624
JoakinsO <32000481+JoakinsO@users.noreply.github.com>
2020-04-11 23:59:08 -0700

    Update index.js

--- services/index.js
+++ services/index.js
@@ -1,8 +1,7 @@
-require('module-alias/register');
 const http = require('http'),
-      BudgetManagerAPI = require('@BudgetManagerAPI'),
+      BudgetManagerAPI = require('./services/BudgetManagerAPI/config'),
       BudgetManagerServer = http.Server(BudgetManagerAPI),
-      BudgetManagerPORT = process.env.PORT || 3001,
+      BudgetManagerPORT = process.env.PORT || 8080,
       LOCAL = '0.0.0.0';
 
 BudgetManagerServer.listen(BudgetManagerPORT, LOCAL, () => console.log(`BudgetManagerAPI running on ${BudgetManagerPORT}`));
